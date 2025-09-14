d28fa76398bc5500fabd8bac2ab1182e2cbc7d0d
satukiARCL <139952702+satukiARCL@users.noreply.github.com>
2025-09-14 11:42:01 +0000

    Update lisstss.sh

--- lisstss.sh
+++ lisstss.sh
@@ -1,5 +1,5 @@
 find ~/novels/ -type f -name "*.txt" -exec cp {} ~/Documents/ \;
 find ~/novels/ -not -name '*.ini' -not -name '*.txt' -not -name '*.rb' -not -name '*.yaml' -not -name '*.yaml.backup' -not -name '*.jpg' -not -name '*.png' -not -name '*.gif' -type f -exec cp {} ~/Documents/LAEGO/ \;
 cd ~/Documents/LAEGO/
-rename -v 's/^([^.]+)$/$1.txt/' *
+find . -maxdepth 1 -type f -exec rename -v 's/^([^.]+)$/$1.txt/' {} +
 find ~/Documents/ -type f -name "*.txt" -exec nkf -sc --overwrite {} \;
